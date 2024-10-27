93b0302952f0cfc78ffb1aa0c57ab16400990a4a
nicolaspark1 <141599069+nicolaspark1@users.noreply.github.com>
2024-10-27 23:11:41 +0000

    final stuff

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,19 +1,88 @@
 class ZebraController < ApplicationController
   def giraffe
-    @random_move = ["rock", "paper", "scissors"].sample
+    player = "rock"
+    computer_choices = ["rock", "paper", "scissors"]
+    computer = computer_choices.sample
 
+    if computer == "rock"
+      result1 = "We played #{player}!"
+      result2 = "They played #{computer}!"
+      result3 = "We tied!"  
+    end
+    if computer == "paper"
+      result1 = "We played #{player}!"
+      result2 = "They played #{computer}!"
+      result3 = "We lost!"  
+    end
+    if computer == "scissors"
+      result1 = "We played #{player}!"
+      result2 = "They played #{computer}!"
+      result3 = "We won!"  
+    end
+
+    @outcome1 = result1
+    @outcome2 = result2
+    @outcome3 = result3
     render({ :template => "game_templates/play_rock" })
   end
 
   def elephant
-    @random_move = ["rock", "paper", "scissors"].sample
+    player = "paper"
+    computer_choices = ["rock", "paper", "scissors"]
+    computer = computer_choices.sample
+
+    if computer == "paper"
+      result1 = "We played #{player}!"
+      result2 = "They played #{computer}!"
+      result3 = "We tied!"  
+    end
+    if computer == "scissors"
+      result1 = "We played #{player}!"
+      result2 = "They played #{computer}!"
+      result3 = "We lost!"  
+    end
+    if computer == "rock"
+      result1 = "We played #{player}!"
+      result2 = "They played #{computer}!"
+      result3 = "We won!"  
+    end
+
+    @outcome1 = result1
+    @outcome2 = result2
+    @outcome3 = result3
 
     render({ :template => "game_templates/play_paper" })
   end
 
   def lion
-    @random_move = ["rock", "paper", "scissors"].sample
+    player = "scissors"
+    computer_choices = ["rock", "paper", "scissors"]
+    computer = computer_choices.sample
+
+    if computer == "scissors"
+      result1 = "We played #{player}!"
+      result2 = "They played #{computer}!"
+      result3 = "We tied!"
+    end
+    if computer == "rock"
+      result1 = "We played #{player}!"
+      result2 = "They played #{computer}!"
+      result3 = "We lost!"  
+    end
+    if computer == "paper"
+      result1 = "We played #{player}!"
+      result2 = "They played #{computer}!"
+      result3 = "We won!"  
+    end
+    @outcome1 = result1
+    @outcome2 = result2
+    @outcome3 = result3
 
     render({ :template => "game_templates/play_scissors" })
   end
+
+  def rules
+    render({ :template => "game_templates/rules" })
+  end
+
 end
